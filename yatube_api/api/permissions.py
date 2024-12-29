06c1d8b3fad59ef8ae8e245eefdc9ac96fc15bdc
Maxim Supreme <chernikovmaks2002@gmail.com>
2024-12-29 20:22:57 +0300

    Update files

--- yatube_api/api/permissions.py
+++ yatube_api/api/permissions.py
@@ -3,8 +3,7 @@ from rest_framework import permissions
 
 class IsAuthor(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
-        if (
+        return (
             request.method in permissions.SAFE_METHODS
             or obj.author == request.user
-        ):
-            return True
+        )
